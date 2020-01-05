1a7f4fc4c83fd25137985c38d751372cf27642b8
Evgeny Yakimov <john.yakimov@gmail.com>
2020-01-05 21:38:42 +0000

    Python Package Boilerplate

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='w3copentracing',
+      version='0.1',
+      description='A partial implementation of an OpenTracing tracer with W3C Trace Context compliance.',
+      url='https://github.com/eyjohn/w3copentracing-python',
+      author='Evgeny Yakimov',
+      author_email='evgeny@evdev.me',
+      license='Apache License Version 2.0',
+      packages=['w3copentracing'],
+      zip_safe=False)
